c55c3fe71b54a119df2d8284b2538fb6cd6a189f
Javier Medina <javiermlovo@hotmail.com>
2020-07-23 18:58:51 -0500

    arreglando e mapa en la pagina de registro

--- Udemy/html/GDL webcam/js/main.js
+++ Udemy/html/GDL webcam/js/main.js
@@ -5,17 +5,15 @@
     document.addEventListener('DOMContentLoaded', function() {
         console.log('ready');
 
-        var map = L.map('mapa').setView([4.630099, -74.090552], 16);
+        var mymap = L.map('mapa').setView([4.630099, -74.090552], 16);
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
+        }).addTo(mymap);
 
-        L.marker([4.630099, -74.090552]).addTo(map)
+        L.marker([4.630099, -74.090552]).addTo(mymap)
             .bindPopup('GDLWebCamp Conference <br> Entradas disponibles')
-            .openPopup()
-            .bindTooltip('GDLWebCamp Conference <br> Entradas disponibles')
-            .openTooltip();
+            .openPopup();
 
         //CAMPOS DATOS USUARIO
         var nombre = document.getElementById('nombre');
